b8839d505fdb2605da450c66b8db56ba60804e89
scarytom <t.denley@cantab.net>
2012-02-13 20:47:47 +0000

    better tests

--- src/test/java/org/netmelody/jarnia/github/GithubFetcherTest.java
+++ src/test/java/org/netmelody/jarnia/github/GithubFetcherTest.java
@@ -1,16 +1,27 @@
 package org.netmelody.jarnia.github;
 
 import org.junit.Test;
+import org.netmelody.jarnia.github.GithubFetcher.FileRef;
 
 import com.google.common.collect.Iterables;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 
 public final class GithubFetcherTest {
+    
+    private final GithubFetcher fetcher = new GithubFetcher();
 
     @Test public void
-    canFetch() {
-        GithubFetcher fetcher = new GithubFetcher();
-        String sha = fetcher.fetchLatestShaFor("netmelody", "ci-eye", "master");
-        System.out.println(Iterables.toString(fetcher.fetchFilesFor("netmelody", "ci-eye", sha)));
+    canFetchLatestSha() {
+        final String sha = fetcher.fetchLatestShaFor("netmelody", "ci-eye", "master");
+        assertThat(sha.length(), is(40));
+    }
+    
+    @Test public void
+    canFetchFileRefs() {
+        final Iterable<FileRef> fileRefs = fetcher.fetchFilesFor("netmelody", "ci-eye", "af8ae09d84624c4fa69c7d405cf254748b8ef152");
+        assertThat(Iterables.size(fileRefs), is(228));
     }
 }
