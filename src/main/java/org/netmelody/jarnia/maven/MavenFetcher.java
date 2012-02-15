94cdd95b4adc0d02688d6b438c594b743d430406
scarytom <t.denley@cantab.net>
2012-02-15 21:04:01 +0000

    maven lives

--- src/main/java/org/netmelody/jarnia/maven/MavenFetcher.java
+++ src/main/java/org/netmelody/jarnia/maven/MavenFetcher.java
@@ -3,17 +3,44 @@ package org.netmelody.jarnia.maven;
 import org.netmelody.jarnia.HttpGetter;
 import org.netmelody.jarnia.github.GithubFetcher.FileRef;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public final class MavenFetcher {
 
     private final HttpGetter getter = new HttpGetter();
+    private final JsonParser jsonParser = new JsonParser();
     
     public String fetchLatestVersionOf(String jarName) {
         final String result = getter.get("http://search.maven.org/solrsearch/select?q=name%3A%22" +
                                          jarName +
                                          "%22%20AND%20type%3A1&rows=100000&core=filelisting&wt=json");
         
-        System.out.println(result);
-        return result;
+        JsonObject response = responseFrom(result);
+        if (null == response) {
+            return "";
+        }
+        
+        
+        final String path = response.get("path").getAsString();
+        String s = path.substring(0, path.length() - jarName.length() - 1);
+        s = s.substring(0, s.lastIndexOf("/"));
+        s = s.substring(0, s.lastIndexOf("/")).replace("/", ".") + ":" + s.substring(s.lastIndexOf("/") + 1, s.length());
+        
+        final String artifact = getter.get("http://search.maven.org/solrsearch/select?q=id:%22" + s + "%22");
+        response = responseFrom(artifact);
+        
+        return response.get("latestVersion").getAsString();
+    }
+
+    private JsonObject responseFrom(final String result) {
+        final JsonObject response = jsonParser.parse(result).getAsJsonObject().get("response").getAsJsonObject();
+        
+        final int numFound = response.get("numFound").getAsInt();
+        if (numFound == 0) {
+            return null;
+        }
+        return response.get("docs").getAsJsonArray().get(0).getAsJsonObject();
     }
     
     public String findVer(FileRef ref) {
--- src/test/java/org/netmelody/jarnia/maven/MavenFetcherTest.java
+++ src/test/java/org/netmelody/jarnia/maven/MavenFetcherTest.java
@@ -2,14 +2,17 @@ package org.netmelody.jarnia.maven;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public final class MavenFetcherTest {
 
     private final MavenFetcher fetcher = new MavenFetcher();
 
     @Test public void
-    canFetchLatestSha() {
-        final String ver = fetcher.fetchLatestVersionOf("guava-11.0.1.jar");
+    canFetchLatestVersion() {
+        final String ver = fetcher.fetchLatestVersionOf("guava-10.0.1.jar");
         
-//        assertThat(sha.length(), is(40));
+        assertThat(ver, is("11.0.1"));
     }
 }
